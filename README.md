614eafe0260ccd107e6d2caf8ae1fd00825a0785
Andy Hamilton <andyhmltn@gmail.com>
2014-03-12 15:52:49 +0000

    Added DNA.js

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ X.JS
 
 It's code in the shape of an X, that when executed, prints out an X :)
 
-This was actually a lot harder than it looks
+This was actually a lot harder than it looks. It also creates a weird pattern
+when you stack it the same code multiple times (see dna.js)
---
+++ dna.js
@@ -0,0 +1,76 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Andy Hamilton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+       true;true;             true;true;
+        true;cons=           console;1;
+         r=function         (tn,stra,l
+         ){return new      Array(tn+1
+          ).join(stra     )};l=s=5;p
+           =0;i=1;ne =false;while(i<
+            8){p+=1;if(!ne&&s<=0){
+             ne=true;}str='';ts=s>
+            0?(r(s-1,' ')):'';if
+           (!(p>=4))     {str+=r(p,
+          ' ')+r(l,      '#')+ts+r(l,
+         '#');cons        .log(str)}if
+        (ne){s+=2;          p-=2;true;}
+        else{s-=2;          }i+=1;};true;
+        true;cons=           console;1;
+         r=function         (tn,stra,l
+         ){return new      Array(tn+1
+          ).join(stra     )};l=s=5;p
+           =0;i=1;ne =false;while(i<
+            8){p+=1;if(!ne&&s<=0){
+             ne=true;}str='';ts=s>
+            0?(r(s-1,' ')):'';if
+           (!(p>=4))     {str+=r(p,
+          ' ')+r(l,      '#')+ts+r(l,
+         '#');cons        .log(str)}if
+        (ne){s+=2;          p-=2;true;}
+        else{s-=2;          }i+=1;};true;
+        true;cons=           console;1;
+         r=function         (tn,stra,l
+         ){return new      Array(tn+1
+          ).join(stra     )};l=s=5;p
+           =0;i=1;ne =false;while(i<
+            8){p+=1;if(!ne&&s<=0){
+             ne=true;}str='';ts=s>
+            0?(r(s-1,' ')):'';if
+           (!(p>=4))     {str+=r(p,
+          ' ')+r(l,      '#')+ts+r(l,
+         '#');cons        .log(str)}if
+        (ne){s+=2;          p-=2;true;}
+        else{s-=2;          }i+=1;};true;
+        true;cons=           console;1;
+         r=function         (tn,stra,l
+         ){return new      Array(tn+1
+          ).join(stra     )};l=s=5;p
+           =0;i=1;ne =false;while(i<
+            8){p+=1;if(!ne&&s<=0){
+             ne=true;}str='';ts=s>
+            0?(r(s-1,' ')):'';if
+           (!(p>=4))     {str+=r(p,
+          ' ')+r(l,      '#')+ts+r(l,
+         '#');cons        .log(str)}if
+        (ne){s+=2;          p-=2;true;}
+        else{s-=2;          }i+=1;};true;
